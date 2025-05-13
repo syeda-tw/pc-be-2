0988bb3789fa1af1ca7f1630b69317b0786e912d
s-zehra <zehrataqwi@gmail.com>
2025-05-13 19:53:14 +0300

    fix: sanitize user send status

--- src/features/common/utils.js
+++ src/features/common/utils.js
@@ -15,8 +15,40 @@ export const generateToken = (payload, expiresIn = "200h") => {
 };
 
 export const sanitizeUserAndAppendType = (user, type) => {
-  const { password, ...userWithoutPassword } = user;
-  return { ...userWithoutPassword, type: type || "user" };
+  const {
+    _id,
+    firstName,
+    lastName,
+    middleName,
+    phone,
+    email,
+    dateOfBirth,
+    gender,
+    pronouns,
+    title,
+    status,
+    // Destructure other fields that might exist but we don't want to include,
+    // especially sensitive ones like password.
+    // eslint-disable-next-line no-unused-vars
+    password,
+    // eslint-disable-next-line no-unused-vars
+    ...rest 
+  } = user;
+
+  return {
+    _id,
+    firstName,
+    lastName,
+    middleName,
+    phone,
+    email,
+    dateOfBirth,
+    gender,
+    pronouns,
+    title,
+    status,
+    type: type || "user",
+  };
 };
 
 export const isPasswordCorrect = async (password, correctPassword) => {
